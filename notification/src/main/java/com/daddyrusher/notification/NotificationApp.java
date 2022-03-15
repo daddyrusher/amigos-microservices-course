d8632aedd0cf3a21c5ed4d1c2c1f8aa4962e6c05
daddyrusher <daddyrusher@gmail.com>
2022-03-15 23:05:12 +0300

    add message producer impl to customer service

--- notification/src/main/java/com/daddyrusher/notification/NotificationApp.java
+++ notification/src/main/java/com/daddyrusher/notification/NotificationApp.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.daddyrusher.notification",
+                "com.daddyrusher.amqp"
+        }
+)
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.daddyrusher.clients"
