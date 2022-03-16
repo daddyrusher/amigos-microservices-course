9a8ba2cee95064a591f5a37b5ddd799ba26e548a
daddyrusher <daddyrusher@gmail.com>
2022-03-16 21:03:18 +0300

    - add packaging - add rabbit consumer in notification service

---
+++ notification/src/main/java/com/daddyrusher/notification/rabbitmq/NotificationConsumer.java
@@ -0,0 +1,21 @@
+package com.daddyrusher.notification.rabbitmq;
+
+import com.daddyrusher.clients.notification.NotificationRequest;
+import com.daddyrusher.notification.NotificationService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+    private final NotificationService notificationService;
+
+    @RabbitListener(queues = "${rabbitmq.queue.notification}")
+    public void consume(NotificationRequest request) {
+        log.info("Consumed {} from queue", request);
+        notificationService.send(request);
+    }
+}
