0b3ab5502a880f9986e8f5b2c1b4a9ebe76466da
daddyrusher <daddyrusher@gmail.com>
2022-09-05 23:52:05 +0300

    - disable eureka - add kube props - pass props into executable apps

--- clients/src/main/java/com/daddyrusher/clients/notification/NotificationClient.java
+++ clients/src/main/java/com/daddyrusher/clients/notification/NotificationClient.java
@@ -3,7 +3,10 @@ package com.daddyrusher.clients.notification;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("notification")
+@FeignClient(
+        name = "notification",
+        url = "${client.notification.url}"
+)
 public interface NotificationClient {
     @PostMapping("api/v1/notification")
     void sendNotification(NotificationRequest notificationRequest);
--- notification/src/main/java/com/daddyrusher/notification/NotificationApp.java
+++ notification/src/main/java/com/daddyrusher/notification/NotificationApp.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -15,6 +17,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients(
         basePackages = "com.daddyrusher.clients"
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApp {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApp.class, args);
